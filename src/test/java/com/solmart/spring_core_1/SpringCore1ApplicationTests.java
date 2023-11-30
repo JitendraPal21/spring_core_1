f8b4bbc1f033f58793347cb142157b398ef9b35a
JitendraPal21 <paljitendra21@gmail.com>
2023-11-30 16:55:16 +0530

    initial commit

---
+++ src/test/java/com/solmart/spring_core_1/SpringCore1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solmart.spring_core_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCore1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
